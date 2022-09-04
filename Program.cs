909b51a44c71989f3389d13f279dd27f6f56e2a8
Petr Popov <petr.v.popov@gmail.com>
2022-09-04 11:13:30 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число a ");
+int a = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите число b ");
+int b = Convert.ToInt32(Console.ReadLine ());
+if (a>b)
+    Console.WriteLine ("Максимальное число - a, минимальное число - b");
+if (b>a)
+    Console.WriteLine ("Максимальное число - b, минимальное число - a");
