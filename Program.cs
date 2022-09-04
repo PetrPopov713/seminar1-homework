d95c5ae7f68768737a1b76de1e45f4e746df020e
PetrPopov713 <106627245+PetrPopov713@users.noreply.github.com>
2022-09-04 11:21:25 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,7 +2,20 @@
 int a = Convert.ToInt32(Console.ReadLine ());
 Console.WriteLine("Введите число b ");
 int b = Convert.ToInt32(Console.ReadLine ());
-if (a>b)
-    Console.WriteLine ("Максимальное число - a, минимальное число - b");
-if (b>a)
-    Console.WriteLine ("Максимальное число - b, минимальное число - a");
+Console.WriteLine("Введите число c ");
+int c = Convert.ToInt32(Console.ReadLine ());
+if 
+  (a>b)
+       if
+         (a>c)
+  Console.WriteLine ("Максимальное число - a");
+if 
+  (b>a)
+       if
+         (b>c)
+  Console.WriteLine ("Максимальное число - b");
+  if 
+  (c>b)
+       if
+         (c>a)
+  Console.WriteLine ("Максимальное число - c");
